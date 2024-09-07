2865fd36cf4e49560d6ddb9a3d98ad53afb3d8e3
Naakinn <fedortar8@gmail.com>
2024-09-07 10:53:11 +0300

    Code refactor Encapsulate VBO names and indexes into struct

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include "glad/glad.h"
 
 #define VERTEXPOSSIZE 64 
+#define VERTEXC 9 
+#define VERTEXATTRSIZE 3
+#define COLORATTRSIZE 3
+
 
 int gWidth = 800; 
 int gHeight = 600; 
@@ -10,29 +14,48 @@ SDL_GLContext* glContext = NULL;
 
 /* VAO */ 
 GLuint glVertexArrayObject = 0; 
-/* VBO */ 
-GLuint glVertexBufferObject = 0; 
-/* Program object */ 
-GLuint glGraphiscPipeLineShaderProgram = 0; 
 
-/* Some data */ 
-const char glVertexShaderSource[] = 
-	"#version 410 core\n"
-	"in vec4 position;\n"
-	"void main()\n"
-    "{\n"
-	"	gl_Position = vec4(position.x, position.y, position.z, position.w);\n"
-	"}\n"
-	;
+/* Grapshics pipeline shader program */ 
+GLuint glPipeLineShaderProgram = 0; 
+
+/* VBOs */ 
+struct VBO {
+	GLuint name; /* VBO name */
+	GLuint idx; /* Index in VertexAttribArray */
+}   glVBOVertex = { 0, 0 },
+	glVBOColor = { 0, 1 };
+
+char* loadShader(const char* filepath) { 
+	FILE* istream = fopen(filepath, "r"); 
 	
-const char glFragmentShaderSource[] = 
-	"#version 410 core\n"
-	"out vec4 color;\n"
-	"void main()\n"
-	"{\n"
-	"	color = vec4(0.8f, 0.65f, 0.97f, 1.0f);\n"
-	"}\n"
-	;
+	if (istream == NULL) { 
+		printf("error occured while opening shader file\n");
+		exit(1); 
+	}
+	
+	size_t size; 
+	char c; 
+	int i; 
+	
+	for (size = 0; (c = fgetc(istream)) != EOF; ++size) 
+		; 
+	char* shaderSource = (char*)malloc(size * sizeof(char) + 1);
+	
+	if (shaderSource == NULL) {
+		printf("malloc err\n"); 
+		exit(1); 
+	}
+	
+	rewind(istream); 
+	for (i = 0; (c = fgetc(istream)) != EOF; ++i) {
+		shaderSource[i] = c; 
+	} 
+	
+	shaderSource[i] = '\0'; 
+	
+	fclose(istream); 
+	return shaderSource;
+}
 	
 GLuint compileShader(GLuint type, const char* source) {
 	GLuint shaderObject; 
@@ -46,11 +69,13 @@ GLuint compileShader(GLuint type, const char* source) {
 	}
 	glShaderSource(shaderObject, 1, &source, 0); 
 	glCompileShader(shaderObject); 
+	free((void*)source); 
 	return shaderObject; 
 }
 
 GLuint createShaderProgram(const char* vertexShaderSource, 
-						 const char* fragmentShaderSource) { 	
+						   const char* fragmentShaderSource) { 	
+	
 	GLuint programObject = glCreateProgram(); 
 	GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource); 
 	GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource); 
@@ -64,7 +89,7 @@ GLuint createShaderProgram(const char* vertexShaderSource,
 	
 }
 
-int main() { 
+int main(int argc, char** argv) {
 	/* Initialize program */ 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("Init err\n"); 
@@ -73,7 +98,6 @@ int main() {
 	/* Set attributes */ 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1); 
-
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); 
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); 
@@ -97,33 +121,62 @@ int main() {
 	/* Vertex specification */ 
 	const GLfloat vertexPositions[VERTEXPOSSIZE] = {
 		-0.8f, -0.8f, 0.0f, 
-		0.8f, -0.8f, 0.0f, 
-		0.0f, 0.8f, 0.0f
+		 0.8f, -0.8f, 0.0f, 
+		 0.0f,  0.8f, 0.0f
+	};
+	const GLfloat vertexColors[VERTEXPOSSIZE] = {
+		 1.0f, 0.0f, 0.0f, 
+		 0.0f, 1.0f, 0.0f, 
+		 0.0f, 0.0f, 1.0f
 	};
 	
-	/* Create VAO */ 
-	glGenVertexArrays(1, &glVertexArrayObject); 
+	/* Generate VAO */ 
+	glGenVertexArrays(1, &glVertexArrayObject);
+	/* Select VAO */
 	glBindVertexArray(glVertexArrayObject); 
 
-	/* Generate VBO */ 
-	glGenBuffers(1, &glVertexBufferObject); 
-	glBindBuffer(GL_ARRAY_BUFFER, glVertexBufferObject); 
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), vertexPositions, GL_STATIC_DRAW);
+	/* Generate vertex VBO */ 
+	glGenBuffers(1, &glVBOVertex.name); 
+	/* Select vertex VBO */
+	glBindBuffer(GL_ARRAY_BUFFER, glVBOVertex.name); 
+	/* Fill vertex VBO with data */ 	
+	glBufferData(GL_ARRAY_BUFFER, VERTEXC * sizeof(GLfloat), vertexPositions, GL_STATIC_DRAW);
 
+	/* Set attributes for vertex VBO */ 
+	glEnableVertexAttribArray(glVBOVertex.idx); 
+	glVertexAttribPointer(glVBOVertex.idx, VERTEXATTRSIZE, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0); 
+	
+	/* Generate color VBO */
+	/* glGenBuffers(1, &glVertexBufferObjectColor);  */
+	/* glBindBuffer(GL_ARRAY_BUFFER, glVertexBufferObjectColor);  */
+	/* glBufferData(GL_ARRAY_BUFFER, VERTEXC * sizeof(GLfloat), vertexColors, GL_STATIC_DRAW); */
+	
 	/* Set attributes */ 
-	glEnableVertexAttribArray(0); 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); 
-
-	/* Shader source */ 
-	glGraphiscPipeLineShaderProgram = createShaderProgram(glVertexShaderSource, 
-														  glFragmentShaderSource);
+	/* glEnableVertexAttribArray(1);  */
+	/* glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);  */
+	
+	/* Unbind selected vertex VBO */
+	/* glBindVertexArray(GL_ZERO);  */
+	/* glDisableVertexAttribArray(glVBOVertex.idx);  */
+	
+	/* Load shaders */ 
+	glPipeLineShaderProgram = createShaderProgram(loadShader("shaders/vert.glsl"), 
+												  loadShader("shaders/frag.glsl"));
+	glUseProgram(glPipeLineShaderProgram); 
+	
+	/* Prepare draw */ 
+	glDisable(GL_DEPTH_TEST); 
+	glDisable(GL_CULL_FACE); 
+	glViewport(0,0, gWidth, gHeight); 
+	glClearColor(0.0f, 0.0f, 0.0f, 1.f); 
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT); 
 	
 	/* Get info */ 
 	printf("Vendor: %s\n", glGetString(GL_VENDOR)); 
 	printf("Renderer: %s\n", glGetString(GL_RENDERER)); 
 	printf("Version: %s\n", glGetString(GL_VERSION));
 	printf("Shading language: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION)); 
-
+	
 	/* Main loop */ 
 	int quit = 0; 
 	while (!quit) {
@@ -134,25 +187,16 @@ int main() {
 				quit = 1; 
 			} 
 		}
-		
-		/* Prepare draw */ 
-		glDisable(GL_DEPTH_TEST); 
-		glDisable(GL_CULL_FACE); 
-		glViewport(0,0, gWidth, gHeight); 
-		glClearColor(1.f, 1.f, 0.f, 1.f); 
-		glUseProgram(glGraphiscPipeLineShaderProgram); 
-
 		/* Draw */ 
-		glBindVertexArray(glVertexArrayObject); 
-		glBindBuffer(GL_ARRAY_BUFFER, glVertexBufferObject); 
-
 		glDrawArrays(GL_TRIANGLES, 0, 3); 
 		
 		SDL_GL_SwapWindow(glWindow); 
 	}
+	
 	/* Exit program */ 
-	glBindVertexArray(0); 
-	glDisableVertexAttribArray(0); 
+	glBindVertexArray(GL_ZERO); 
+	glDisableVertexAttribArray(glVBOVertex.idx); 
+	/* glDisableVertexAttribArray(1);  */
 
 	SDL_DestroyWindow(glWindow); 
 	SDL_Quit(); 
---
+++ shaders/frag.glsl
@@ -0,0 +1,6 @@
+#version 410 core
+out vec4 color;
+void main()
+{
+	color = vec4(0.8f, 0.65f, 0.97f, 1.0f);
+}
